da0d5f30420752ddf6b79cde0a1bfbb7323fdff0
Seth Velasquez <sethvelasquez@Seths-MacBook-Pro.local>
2023-11-07 08:54:35 -0700

    added initial form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import LogIn from './components/InitialForm';
+export const newUser = {
+  customerName: '',
+  customerIncome: 0
+};
+export const appData = {
+  newUser: newUser
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <LogIn />
     </div>
   );
 }
