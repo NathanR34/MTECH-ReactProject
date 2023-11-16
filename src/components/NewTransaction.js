090ab5a56db5052e48fa6cde59f05d9b2c809e43
Seth <urchen46@gmail.com>
2023-11-16 09:23:14 -0700

    added a user choice of income or expense

--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -1,19 +1,66 @@
-// import {historyArr} from "../ExpenseTracker"
-
+import {User} from '../App'
 
 export default function NewTransaction({addTransaction}){
 
+    let expenseIncomeHandler = null
     
 
-    const addNewTransaction = (e) => {
-        const newTran = {
-            title: e.target.previousSibling.previousSibling.previousSibling.value,
-            amount: e.target.previousSibling.value
+    const handler = () => {
+        const expense = document.querySelector('.expenseCB');
+        const income = document.querySelector('.incomeCB');
+        if (expense.checked === true){
+            expenseIncomeHandler = 'subtract' 
+            income.checked = false
         }
-        e.target.previousSibling.previousSibling.previousSibling.value = ""
-        e.target.previousSibling.value = ""
-        // historyArr.push(newTran)
-        addTransaction(newTran)
+        if (income.checked === true){
+            expenseIncomeHandler = 'add'
+            expense.checked = false
+        }
+    }
+
+    const checkboxHandler = () => {
+        const expense = document.querySelector('.expenseCB');
+        const income = document.querySelector('.incomeCB');
+        if (expense.checked === true){
+            income.checked = false
+        }
+        if (income.checked === true){
+            expense.checked = false
+        }
+    }
+
+     const handleExpense = (e) => {
+        e.target.nextSibling.nextSibling.checked = false
+    }
+    const handleIncome = (e) => {
+        e.target.previousSibling.previousSibling.checked = false
+    }
+
+    const addNewTransaction = (e) => {
+        handler()
+
+        const title = e.target.previousSibling.previousSibling.previousSibling.previousSibling.value
+        const amount = e.target.previousSibling.previousSibling.value
+        if(title && amount){
+            const newTran = {
+                title: title,
+                amount: amount
+            }
+            e.target.previousSibling.previousSibling.previousSibling.previousSibling.value = ""
+            e.target.previousSibling.previousSibling.value = ""
+            addTransaction(newTran)
+            if (expenseIncomeHandler === 'add'){
+                User.cash = Number(User.cash) + Number(newTran.amount) 
+            }
+            else if (expenseIncomeHandler === 'subtract'){
+                User.cash = User.cash - newTran.amount 
+            }
+        }
+        else {
+            alert('Please put a title and amount for your transaction')
+        }
+
+        console.log(expenseIncomeHandler)
     }
 
     return (
@@ -22,7 +69,13 @@ export default function NewTransaction({addTransaction}){
             <input></input>
             <label>Amount</label>
             <input type="number"></input>
-            <button onClick={addNewTransaction} ></button>
+            
+            <div>
+                <p>Select Expense or Income</p>
+                <input type='checkbox' className='expenseCB' onClick={handleExpense}  />Expense
+                <input type='checkbox' className='incomeCB' onClick={handleIncome} />Income
+            </div>
+            <button onClick={addNewTransaction}>Add</button>
         </div>
     )
 }
