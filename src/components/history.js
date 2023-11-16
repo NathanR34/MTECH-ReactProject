71a5d68bbaf4edff96dc4e7121fd96bcd8b64189
Seth <urchen46@gmail.com>
2023-11-16 10:10:04 -0700

    changed selecting values from siblings to document.quearyselector

--- src/components/history.js
+++ src/components/history.js
@@ -2,19 +2,26 @@
 
 
 export default function History({historyArr}){
-
-    console.log(historyArr)    
+  
     // if(historyArr !== []){
     //     return(
     //         <div>No history yet, please add some transactions to see it here.</div>
     //     )
     // }
 
+    const historyOverview = historyArr.slice(-4)
+
+    console.log()
+
+    // console.log(historyArr[historyArr.length - 1])
+    // historyArr = ([historyArr[historyArr.length - 1]])
+    
+
     return (
         <div className='spending-history'>
         <h2>History:</h2>
         <ul>
-            {historyArr.map((tran, index) => (
+            {historyOverview.map((tran, index) => (
                 <li key={index} >{tran.title}: {tran.amount}</li>
             ))}
         </ul>
